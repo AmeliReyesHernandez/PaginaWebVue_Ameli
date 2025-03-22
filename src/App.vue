bc477d5a7f01e80389d7be811f9af22044ade1c8
AmeliReyesHernandez <22620050@tlaxiaco.tecnm.mx>
2025-03-22 02:21:26 -0600

    Agregue Carrusel

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Navbar from "/src/components/Navbar.vue"
 import Descripcion from "/src/components/Descripcion.vue"
+import Carrusel from "/src/components/Carrusel.vue"
 </script>
 
 <template>
   
   <Navbar />
   <Descripcion />
+  <Carrusel />
 
 </template>
 
